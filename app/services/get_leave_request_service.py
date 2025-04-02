09362d3dd296e003bad71202ed8209a73a55e26b
Tanushree Sharma <66589482+TanuShree952838@users.noreply.github.com>
2025-04-02 16:54:21 +0530

    Update and rename get_leave_request_service to get_leave_request_service.py

--- app/services/get_leave_request_service.py
+++ app/services/get_leave_request_service.py
@@ -2,4 +2,4 @@ from app.repository.leave_requests import get_leave
 
 def get_leave_request_service(db, employee_id):
     """Service to fetch leave data of individual employee based on employee_id"""
-    get_leave(db,employee_id)
+    return get_leave(db,employee_id)
